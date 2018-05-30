d106dfdfb1bd7f859dcf5a3ae02911c4cac9be00
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-05-30 22:38:17 +0300

    Lection1 HW1

---
+++ CSharpHW/1/HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        private const string Name = "Maxim";
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}", Name);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
