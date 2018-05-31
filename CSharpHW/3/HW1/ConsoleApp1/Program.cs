42af30e35b3f88b3c22e0bddca7ee21543e9d925
120-W10-1803-VS\1 <maksimkhanin.kpi@gmail.com>
2018-05-31 12:09:55 +0300

    Lection3 HW1 in process

---
+++ CSharpHW/3/HW1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registrationForm = new RegistrationForm {FirstName = "Maxim"};
+            Console.WriteLine(registrationForm.FirstName);
+            Validate(registrationForm);
+
+        }
+        private static void Validate(RegistrationForm registrationForm)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(registrationForm);
+            if (!Validator.TryValidateObject(registrationForm, context, results, true))
+            {
+                foreach (var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("User '{0} {1}' is Valid", registrationForm.FirstName, registrationForm.LastName);
+            }
+        }
+    }
+}
---
+++ CSharpHW/3/HW1/HW1/RegistrationForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HW1
+{
+    public class RegistrationForm
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AdditionalInfo { get; set; }
+    }
+}
